5ff38aa4265544f62c64f78a5684b2c89df79741
Sriram19g <sriramg.cs2023@citchennai.net>
2024-09-04 17:44:23 +0530

    [FEATURE] redirect short url to correct urrl [

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,json,redirect
+from url_generator import save_url_to_json
 
 app = Flask(__name__)
+host="http://127.0.0.1:2006/"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
         url_received = request.form["lurl"]
+        short_url=save_url_to_json(url_received,'user123')
+        print(host+short_url)
+        return host+short_url
        
-        return url_received
+        
     else:
         return render_template("home.html")
 
+@app.route('/<short_url>', methods=['GET'])
+def redirecting(short_url):
+    with open('urls.json','r') as file:
+        data=json.load(file)
+        if short_url in data:
+            l_url=data[short_url].get('long_url')
+            print(f"long url : {l_url}")
+            return redirect(short_url)
+        else:
+            abort(404,description="Short URL not found")
+
 
 if __name__ == '__main__':
-    app.run(port=9999, debug=True)
+    app.run(debug=True,port=2006)
