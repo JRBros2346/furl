858674eb4cd616b32678e944a28e1f5871226b0f
Sriram19g <sriramg.cs2023@citchennai.net>
2024-09-04 13:40:42 +0530

    [MODIFIED] app.py😁

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        url_received = request.form["lurl"]
+        # Logic for shortening the URL would go here
+        return url_received
+    else:
+        return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(port=9999, debug=True)
