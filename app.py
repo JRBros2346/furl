b3887cfed0949f43da10fc1e043677ec33a629a6
JRBros2346 <jrbros2346@gmail.com>
2024-09-05 14:09:37 +0530

    [Fix] Mini Fixes 🪛

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session, json, abort,jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, json, abort
 from url_generator import save_url_to_json, load_user_urls
-import os,hashlib
+import os, hashlib
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+# app.secret_key = 'your_secret_key'
 host = "http://127.0.0.1:5000/"
 
 def hash_password(password):
@@ -12,44 +12,41 @@ def hash_password(password):
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
-        u_name = request.form["username"]
-        passwd = request.form["password"]
+        username = request.form["username"]
+        password = request.form["password"]
 
         if os.path.exists('user.json') and os.path.getsize('user.json') > 0:
             with open('user.json', 'r') as info:
                 data = json.load(info)
-                if u_name in data and data[u_name]['password'] == hash_password(passwd):
-                    session['username'] = u_name
-                    return redirect(url_for('home', username=u_name))
-
+                if username in data and data[username]['password'] == hash_password(password):
+                    session['username'] = username
+                    return redirect(url_for('home', username=username))
         return render_template("login.html", error="Invalid username or password")
 
     return render_template("login.html")
 
-@app.route('/create_account', methods=['GET', 'POST'])
-def create_account():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     if request.method == 'POST':
-        u_name = request.form["username"]
-        passwd = request.form["password"]
+        username = request.form["username"]
+        password = request.form["password"]
 
-        encrypted_passwd = hash_password(passwd)
+        hashed_password = hash_password(password)
         if os.path.exists('user.json') and os.path.getsize('user.json') > 0:
             with open('user.json', 'r') as info:
                 data = json.load(info)
         else:
             data = {}           
-        if u_name not in data:
-            data[u_name] = {'password': encrypted_passwd}
+        if username not in data:
+            data[username] = {'password': hashed_password}
             with open('user.json', 'w') as info:
                 json.dump(data, info, indent=4)
             return redirect(url_for('login'))
 
-        return render_template("create_account.html", error="Username already exists")
+        return render_template("signup.html", error="Username already exists")
 
-    return render_template("create_account.html")
+    return render_template("signup.html")
 
-@app.route('/home/<username>', methods=['GET', 'POST'])
-@app.route('/home/<username>', methods=['GET', 'POST'])
 @app.route('/home/<username>', methods=['GET', 'POST'])
 def home(username):
     if 'username' not in session or session['username'] != username:
@@ -82,8 +79,8 @@ def redirecting(short_url):
             abort(404, description="Short URL not found")
 
 
-@app.route('/deactivate/<short_url>', methods=['POST'])
-def deactivate_url(short_url):
+@app.route('/delete/<short_url>', methods=['POST'])
+def delete_url(short_url):
     if 'username' not in session:
         return redirect(url_for('login'))
     
@@ -91,7 +88,7 @@ def deactivate_url(short_url):
     with open('urls.json', 'r') as file:
         data = json.load(file)
     
-    # Ensure that only the URL created by the user is deactivated
+    # Ensure that only the URL created by the user is deleted
     if short_url in data and data[short_url]['username'] == username:
         del data[short_url]
         
