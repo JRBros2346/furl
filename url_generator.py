3b0bac4647bffc859927c4a93a473ab4e0d8ac32
Sriram19g <sriramg.cs2023@citchennai.net>
2024-09-05 11:28:29 +0530

    [Update] Url_generator.py🖇️

--- url_generator.py
+++ url_generator.py
@@ -1,4 +1,4 @@
-import random, string, json, os
+import random,string,json,os
 
 def generate_short_url(existing_urls, length=6):
     while True:
@@ -6,7 +6,7 @@ def generate_short_url(existing_urls, length=6):
         if short_url not in existing_urls:
             return short_url
 
-def save_url_to_json(long_url, username, json_file='urls.json'):
+def save_url_to_json(long_url, username, title, json_file='urls.json'):
     if os.path.exists(json_file):
         if os.path.getsize(json_file) > 0:
             with open(json_file, 'r') as file:
@@ -19,14 +19,22 @@ def save_url_to_json(long_url, username, json_file='urls.json'):
     else:
         url_data = {}
 
-    for short_url, details in url_data.items():
-        if details['long_url'] == long_url and details['username'] == username:
-            return short_url  
-
     short_url = generate_short_url(url_data.keys())
-    url_data[short_url] = {"long_url": long_url, "username": username, "count": 0}
+    
+    if username not in url_data:
+        url_data[username] = {}
+    
+    url_data[username][short_url] = {"long_url": long_url, "title": title, "count": 0}
     
     with open(json_file, 'w') as file:
         json.dump(url_data, file, indent=4)
     
     return short_url
+
+def load_user_urls(username, json_file='urls.json'):
+    if os.path.exists(json_file) and os.path.getsize(json_file) > 0:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+            if username in data:
+                return data[username]
+    return {}
